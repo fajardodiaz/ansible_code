3fc2e166db7c6ceedb41d3cf0305b6f2d56241a5
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-11-23 02:08:56 -0600

    Fixing issues

--- edit-jmeter-props.sh
+++ edit-jmeter-props.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-MY_STRING="remote_hosts=10.0.0.1,10.0.0.2"
+MY_STRING="remote_hosts=11.0.1.84,11.0.1.53"
 sudo sed -i "s/remote_hosts=127.0.0.1/$MY_STRING/" /opt/apache-jmeter-5.5/bin/jmeter.properties
